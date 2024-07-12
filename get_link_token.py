39fd5ca7e965542b2a2389a8bd98135b21d1ac6e
Mike Earley <mike@5sn1.com>
2024-07-12 09:31:51 -0400

    feature: get link token from Plaid using python module.

--- get_link_token.py
+++ get_link_token.py
@@ -1,39 +1,43 @@
 import os
+from dotenv import load_dotenv
+from dotenv import set_key, dotenv_values
 import plaid
 from plaid.api import plaid_api
 from plaid.model.link_token_create_request import LinkTokenCreateRequest
 from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
+from plaid.model.country_code import CountryCode
+from plaid.model.products import Products
 
-# load env vars from .env file
-from dotenv import load_dotenv
+# Load environment variables from .env file
 load_dotenv()
 
 PLAID_CLIENT_ID = os.getenv('PLAID_CLIENT_ID')
 PLAID_SECRET = os.getenv('PLAID_SECRET')
 PLAID_ENVIRONMENT = os.getenv('PLAID_ENVIRONMENT', 'sandbox')
+ENV_PATH = '.env'
 
-# config
+# Configure Plaid client
 configuration = plaid.Configuration(
     host=plaid.Environment.Sandbox,
     api_key={
-        'clientId': PLAID_CLIENT_ID, 
+        'clientId': PLAID_CLIENT_ID,
         'secret': PLAID_SECRET,
         'plaidVersion': '2020-09-14'
     }
 )
 
-# initialize client
+# Initialize client
 api_client = plaid.ApiClient(configuration)
 client = plaid_api.PlaidApi(api_client)
 
-# create link token
+# Create link token
 request = LinkTokenCreateRequest(
     user=LinkTokenCreateRequestUser(
         client_user_id='unique_user_id'
     ),
     client_name='BalanceIt',
-    products=['auth', 'transactions'],
-    country_codes=['US'],
+    products=[Products('auth'), Products('transactions')],
+    country_codes=[CountryCode('US')],
     language='en'
 )
 
@@ -41,3 +45,11 @@ response = client.link_token_create(request)
 link_token = response['link_token']
 
 print(f'Link token: {link_token}')
+
+# Save the Link token to .env file without quotes
+current_values = dotenv_values(ENV_PATH)
+current_values['PLAID_LINK_TOKEN'] = link_token
+
+with open(ENV_PATH, 'w') as env_file:
+    for key, value in current_values.items():
+        env_file.write(f"{key}={value}\n")
